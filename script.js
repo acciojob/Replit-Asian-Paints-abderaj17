41ca85cba1bb1571eefbb4a113e098d2db92c060
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-22 16:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-document.getElementById('change_button').addEventListener('click', 
-function(){
-	var blockId = document.getElementById('block_id').value;
-	var color = document.getElementById('colour_id').value;
+window.onload = function() {
+  document.getElementById('change_button').addEventListener('click', 
+  function(){
+    var blockId = document.getElementById('block_id').value;
+    var color = document.getElementById('colour_id').value;
 
-	var blocks = document.getElementsByClassName('grid-item');
-	for(var i = 0; i<blocks.length; i++){
-		blocks[i].style.backgroundColor = 'transparent';
-	}
-	document.getElementById(blockId).style.backgroundColor = color;
-});
+    var blocks = document.getElementsByClassName('grid-item');
+    for(var i = 0; i<blocks.length; i++){
+      blocks[i].style.backgroundColor = 'transparent';
+    }
+    document.getElementById(blockId).style.backgroundColor = color;
+  });
 
-document.getElementById('Reset').addEventListener('click', function(){
-	var blocks = document.getElementsByClassName('grid-item');
-	for(let i = 0; i < blocks.length; i++){
-		blocks[i].style.backgroundColor = 'transparent';
-	}
-});
+  document.getElementById('Reset').addEventListener('click', function(){
+    var blocks = document.getElementsByClassName('grid-item');
+    for(let i = 0; i < blocks.length; i++){
+      blocks[i].style.backgroundColor = 'transparent';
+    }
+  });
+}
