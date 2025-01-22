5e54ca4c0709b04928f63303abd0b3b499dd3c00
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-22 16:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', 
+function(){
+	var blockId = document.getElementById('block_id').value;
+	var color = document.getElementById('color_id').value;
+
+	var blocks = document.getElementsByClassName('grid-item');
+	for(var i = 0; i<blocks.length; i++){
+		blocks[i].style.backgroundColor = 'transparent';
+	}
+	document.getElementById(blockId).style.backgroundColor = color;
+});
+
+document.getElementById('Reset').addEventListener('click', funtion(){
+	var blocks = document.getElementByClassName('grid-item');
+	for(let i = 0; i < blocks.length; i++){
+		blocks[i].style.backgroundColor = 'transparent';
+	}
+});
